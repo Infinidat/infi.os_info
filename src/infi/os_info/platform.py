b37eba2a61c40899e6bfacc12f29ee9243893a1c
Kobi Tal <ktal@infinidat.com>
2019-11-18 17:10:23 +0200

    HOSTDEV-3235 Using distro.linux_distribution instead of platform.linux_distribution

---
+++ src/infi/os_info/platform.py
@@ -0,0 +1,11 @@
+# Module with same api as built-in platform but different implementation of linux_distribution that uses distro's one.
+from __future__ import absolute_import
+from platform import *
+
+
+def linux_distribution():
+    import distro
+    id_name, version, codename = distro.linux_distribution(full_distribution_name=False)
+    # distro returns rhel instead of redhat and sles instead of suse
+    id_name.replace('rhel', 'redhat').replace('sles', 'suse')
+    return (id_name, version, codename)
